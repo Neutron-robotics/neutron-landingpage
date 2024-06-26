a8e53f9b70b53cbc97e5d75b1c98a811a63729e2
hugo <hugo.perier@cern.ch>
2024-06-26 18:22:03 +0200

    wip api

--- home/next.config.mjs
+++ home/next.config.mjs
@@ -5,20 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-
-  serverRuntimeConfig: {
-    NEXT_PUBLIC_NEUTRON_WEB: process.env.NEXT_PUBLIC_NEUTRON_WEB,
-    NEXT_PUBLIC_NEUTRON_API: process.env.NEXT_PUBLIC_NEUTRON_API,
-    NEXT_PUBLIC_NEUTRON_DOCS: process.env.NEXT_PUBLIC_NEUTRON_DOCS,
-    NEXT_PUBLIC_NEUTRON_APPLICATION: process.env.NEXT_PUBLIC_NEUTRON_APPLICATION,
-  },
-  // Will be available on both server and client
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_NEUTRON_WEB: process.env.NEXT_PUBLIC_NEUTRON_WEB,
-    NEXT_PUBLIC_NEUTRON_API: process.env.NEXT_PUBLIC_NEUTRON_API,
-    NEXT_PUBLIC_NEUTRON_DOCS: process.env.NEXT_PUBLIC_NEUTRON_DOCS,
-    NEXT_PUBLIC_NEUTRON_APPLICATION: process.env.NEXT_PUBLIC_NEUTRON_APPLICATION,
-  },
 };
 
 export default withNextIntl(nextConfig);
