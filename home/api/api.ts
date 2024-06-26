07c716cf5744b9d8a69d6af40cc47d3c509df047
hugo <hugo.perier@cern.ch>
2024-06-26 21:13:53 +0200

    test env

--- home/api/api.ts
+++ home/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_NEUTRON_API, 
+  baseURL: "https://api.neutron-robotics.com", 
   headers: {
     timeout : 1000,
   }
